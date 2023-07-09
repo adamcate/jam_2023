f90bb154692e1b555761ce86fb51a5ff7e066795
Adam <94191657+pepsiman3@users.noreply.github.com>
2023-07-08 23:32:29 -0600

    add potentially better curve seg representation

--- include/curve.h
+++ include/curve.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "vec.h"
 
+typedef struct segment_2d_s{
+    vec2 p1;
+    vec2 p2;
+    vec2 p3;
+} segment_2d;
+
 vec2 quad_interpolate_2d(vec2 points[3], float t);
 vec3 quad_interpolate_3d(vec3 points[3], float t);
