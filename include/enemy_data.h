1a2a1fef095c4e2fa06c6f76b0e9e3756a84243c
Adam <94191657+pepsiman3@users.noreply.github.com>
2023-07-19 22:06:25 -0600

    modify bullet structure

--- include/enemy_data.h
+++ include/enemy_data.h
@@ -7,18 +7,34 @@ typedef struct enemy_s enemy;
 
 typedef enum enemy_type_e{
     ENEMY_PLACEHOLDER,
-    ENEMY_SCOUT_SHIP
+    ENEMY_SCOUT_SHIP,
+    ENEMY_FIRE_TEST
 } enemy_type;
 
 typedef enum bullet_type_e{
     BULLET_WATER_ABSORB,
-    BULLET_LASER
+    BULLET_LASER,
+    BULLET_PLAYER_SPAZER
 } bullet_type;
+
+typedef struct bullet_s{
+    bullet_type type;
+    circle_t collider;
+    vec2 position;
+    bool enabled;
+} bullet;
 
 typedef struct enemy_placeholder_s{
     circle_t collider;
     int health;
 } enemy_placeholder;
+
+typedef struct enemy_fire_test_data_s{
+    circle_collider collider;
+    int health;
+
+    bullet *bullets;
+} enemy_fire_test_data;
 
 typedef struct enemy_turret_data_s{
     float angle;
@@ -29,7 +45,6 @@ typedef struct enemy_turret_data_s{
 
     float spread_angle;
     float fire_rate;
-
     int spread_amount;
             
     enemy *root_enemay;
